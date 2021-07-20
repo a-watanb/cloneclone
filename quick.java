9c9d3c99669470577dc1f96b1d2e42381c63f101
渡部 綾 <watanabeaya@watanabenoMacBook-Pro.local>
2021-07-21 01:53:48 +0900

    Third commit

---
+++ quick.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int[] data = {5, 10, 3, 7, 8, 1, 9};
+        print_data(data);
+        quick_sort(data, 0, data.length-1);
+        print_data(data);
+	}
+	static void print_data(int[] d) {
+        for(int i = 0; i < d.length; i++) System.out.print(d[i] + " ");
+        System.out.println();
+    }
+    static void quick_sort(int[] d, int left, int right) {
+        if (left>=right) {
+            return;
+        }
+        int p = d[(left+right)/2];
+        int l = left, r = right, tmp;
+        while(l<=r) {
+            while(d[l] < p) { l++; }
+            while(d[r] > p) { r--; }
+            if (l<=r) {
+                tmp = d[l]; d[l] = d[r]; d[r] = tmp;
+                l++; r--;
+            }
+        }
+        quick_sort(d, left, r);  // ピボットより左側をクイックソート
+        quick_sort(d, l, right); // ピボットより右側をクイックソート
+    }
+}
